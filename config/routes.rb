9a454a582ee656d22688cd3221a6675241b67970
akosua kernizan <akernizan@gmail.com>
2015-04-03 10:36:54 -0400

    consol files

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root :to => 'games#home'
+
+  get 'heights' => 'games#heights', as: :heights
+
+  get 'small' => 'games#small', as: :small
+
+  get 'dark' => 'games#dark', as: :dark 
+end
